liufei2008/LGUI

分离出LGUIHudVertex并改名为LGUIMeshVertex

Source/LGUI/Private/Core/ActorComponent/UIBatchGeometryRenderable.cpp
@@ -484,7 +484,7 @@ void ULGUIGeometryHelper::AddVertexSimple(FVector position, FColor color, FVecto
 	auto& originVertices = UIGeo->originVertices;
 	originVertices.Add(position);
 	auto& vertices = UIGeo->vertices;
-	FLGUIHudVertex vert(FVector::ZeroVector);
+	FLGUIMeshVertex vert(FVector::ZeroVector);
 	vert.Color = color;
 	vert.TextureCoordinate[0] = uv0;
 	vertices.Add(vert);
@@ -508,7 +508,7 @@ void ULGUIGeometryHelper::AddVertexFull(FVector position, FColor color, FVector2
 	auto& originVertices = UIGeo->originVertices;
 	originVertices.Add(FLGUIOriginVertexData(position, normal, tangent));
 	auto& vertices = UIGeo->vertices;
-	FLGUIHudVertex vert(FVector::ZeroVector);
+	FLGUIMeshVertex vert(FVector::ZeroVector);
 	vert.Color = color;
 	vert.TextureCoordinate[0] = uv0;
 	vert.TextureCoordinate[1] = uv1;
@@ -535,7 +535,7 @@ void ULGUIGeometryHelper::AddVertexStruct(FLGUIGeometryVertex vertex)
 	auto& originVertices = UIGeo->originVertices;
 	originVertices.Add(FLGUIOriginVertexData(vertex.position, vertex.normal, vertex.tangent));
 	auto& vertices = UIGeo->vertices;
-	FLGUIHudVertex vert(FVector::ZeroVector);
+	FLGUIMeshVertex vert(FVector::ZeroVector);
 	vert.Color = vertex.color;
 	vert.TextureCoordinate[0] = vertex.uv0;
 	vert.TextureCoordinate[1] = vertex.uv1;
@@ -658,7 +658,7 @@ void ULGUIGeometryHelper::AddVertexTriangleStream(const TArray<FLGUIGeometryVert
 	{
 		auto& originVert = InVertexTriangleStream[i];
 		originVertices.Add(FLGUIOriginVertexData(originVert.position, originVert.normal, originVert.tangent));
-		FLGUIHudVertex vert(FVector::ZeroVector);
+		FLGUIMeshVertex vert(FVector::ZeroVector);
 		vert.Color = originVert.color;
 		vert.TextureCoordinate[0] = originVert.uv0;
 		vert.TextureCoordinate[1] = originVert.uv1;

Source/LGUI/Public/Core/LGUIMeshVertex.h
@@ -1,0 +1,50 @@
+﻿// Copyright 2019-2022 LexLiu. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RenderResource.h"
+#include "PackedNormal.h"
+
+#define LGUI_VERTEX_TEXCOORDINATE_COUNT 4
+
+struct LGUI_API FLGUIMeshVertex
+{
+	FLGUIMeshVertex(){}
+	FLGUIMeshVertex(const FVector& InPosition):
+		Position(InPosition),
+		Color(FColor(255, 255, 255)),
+		TangentX(FVector(1, 0, 0)),
+		TangentZ(FVector(0, 0, 1))
+	{
+		// basis determinant default to +1.0
+		TangentZ.Vector.W = 127;
+
+		for (int i = 0; i < LGUI_VERTEX_TEXCOORDINATE_COUNT; i++)
+		{
+			TextureCoordinate[i] = FVector2D::ZeroVector;
+		}
+	}
+
+	FVector Position;
+	FColor Color;
+	FVector2D TextureCoordinate[LGUI_VERTEX_TEXCOORDINATE_COUNT];
+	FPackedNormal TangentX;
+	FPackedNormal TangentZ;
+
+	FVector GetTangentY() const
+	{
+		return GenerateYAxis(TangentX, TangentZ);
+	};
+};
+
+class LGUI_API FLGUIMeshVertexDeclaration : public FRenderResource
+{
+public:
+	FVertexDeclarationRHIRef VertexDeclarationRHI;
+	virtual ~FLGUIMeshVertexDeclaration() {}
+	virtual void InitRHI()override;
+	virtual void ReleaseRHI()override;
+};
+LGUI_API FVertexDeclarationRHIRef& GetLGUIMeshVertexDeclaration();
+

TypeDreamMoon/DreamGUI

Source/LGUI/Private/Core/ActorComponent/UIBatchGeometryRenderable.cpp
@@ -484,7 +484,7 @@ void ULGUIGeometryHelper::AddVertexSimple(FVector position, FColor color, FVecto
 	auto& originVertices = UIGeo->originVertices;
 	originVertices.Add(FVector3f(position));
 	auto& vertices = UIGeo->vertices;
-	FLGUIHudVertex vert(FVector3f::ZeroVector);
+	FLGUIMeshVertex vert(FVector3f::ZeroVector);
 	vert.Color = color;
 	vert.TextureCoordinate[0] = FVector2f(uv0);
 	vertices.Add(vert);
@@ -508,7 +508,7 @@ void ULGUIGeometryHelper::AddVertexFull(FVector position, FColor color, FVector2
 	auto& originVertices = UIGeo->originVertices;
 	originVertices.Add(FLGUIOriginVertexData((FVector3f)position, (FVector3f)normal, (FVector3f)tangent));
 	auto& vertices = UIGeo->vertices;
-	FLGUIHudVertex vert(FVector3f::ZeroVector);
+	FLGUIMeshVertex vert(FVector3f::ZeroVector);
 	vert.Color = color;
 	vert.TextureCoordinate[0] = FVector2f(uv0);
 	vert.TextureCoordinate[1] = FVector2f(uv1);
@@ -535,7 +535,7 @@ void ULGUIGeometryHelper::AddVertexStruct(FLGUIGeometryVertex vertex)
 	auto& originVertices = UIGeo->originVertices;
 	originVertices.Add(FLGUIOriginVertexData((FVector3f)vertex.position, (FVector3f)vertex.normal, (FVector3f)vertex.tangent));
 	auto& vertices = UIGeo->vertices;
-	FLGUIHudVertex vert(FVector3f::ZeroVector);
+	FLGUIMeshVertex vert(FVector3f::ZeroVector);
 	vert.Color = vertex.color;
 	vert.TextureCoordinate[0] = FVector2f(vertex.uv0);
 	vert.TextureCoordinate[1] = FVector2f(vertex.uv1);
@@ -658,7 +658,7 @@ void ULGUIGeometryHelper::AddVertexTriangleStream(const TArray<FLGUIGeometryVert
 	{
 		auto& originVert = InVertexTriangleStream[i];
 		originVertices.Add(FLGUIOriginVertexData((FVector3f)originVert.position, (FVector3f)originVert.normal, (FVector3f)originVert.tangent));
-		FLGUIHudVertex vert(FVector3f::ZeroVector);
+		FLGUIMeshVertex vert(FVector3f::ZeroVector);
 		vert.Color = originVert.color;
 		vert.TextureCoordinate[0] = FVector2f(originVert.uv0);
 		vert.TextureCoordinate[1] = FVector2f(originVert.uv1);

Source/LGUI/Public/Core/LGUIMeshVertex.h
@@ -1,0 +1,50 @@
+﻿// Copyright 2019-2022 LexLiu. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RenderResource.h"
+#include "PackedNormal.h"
+
+#define LGUI_VERTEX_TEXCOORDINATE_COUNT 4
+
+struct LGUI_API FLGUIMeshVertex
+{
+	FLGUIMeshVertex(){}
+	FLGUIMeshVertex(const FVector3f& InPosition):
+		Position(InPosition),
+		Color(FColor(255, 255, 255)),
+		TangentX(FVector3f(1, 0, 0)),
+		TangentZ(FVector3f(0, 0, 1))
+	{
+		// basis determinant default to +1.0
+		TangentZ.Vector.W = 127;
+
+		for (int i = 0; i < LGUI_VERTEX_TEXCOORDINATE_COUNT; i++)
+		{
+			TextureCoordinate[i] = FVector2f::ZeroVector;
+		}
+	}
+
+	FVector3f Position;
+	FColor Color;
+	FVector2f TextureCoordinate[LGUI_VERTEX_TEXCOORDINATE_COUNT];
+	FPackedNormal TangentX;
+	FPackedNormal TangentZ;
+
+	FVector3f GetTangentY() const
+	{
+		return FVector3f(GenerateYAxis(TangentX, TangentZ));
+	};
+};
+
+class LGUI_API FLGUIMeshVertexDeclaration : public FRenderResource
+{
+public:
+	FVertexDeclarationRHIRef VertexDeclarationRHI;
+	virtual ~FLGUIMeshVertexDeclaration() {}
+	virtual void InitRHI()override;
+	virtual void ReleaseRHI()override;
+};
+LGUI_API FVertexDeclarationRHIRef& GetLGUIMeshVertexDeclaration();
+